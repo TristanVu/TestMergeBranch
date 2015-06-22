a9198471dca932753d2799b36056e654a7aa13f0
Tristan <tristan@enclave.vn>
2015-06-22 15:59:52 +0700

    Removing copy right

--- Java/ImportManager.java
+++ Java/ImportManager.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2015 Clare Controls. All rights reserved.
- *
- * This software is the confidential and proprietary information of Clare Controls ("Confidential Information"). You
- * shall not disclose or reproduce such Confidential Information and shall use it only in accordance with the terms of
- * the license agreement you entered into with Clare Controls.
- */
 package com.clarecontrols.equator.solstice.api.beta1;
 
 import java.util.ArrayList;
