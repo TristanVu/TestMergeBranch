4c0b4ee126ec87f25c0e1856019f1bf703c76e0b
Tristan <tristan@enclave.vn>
2015-06-22 14:08:40 +0700

    Adding 3 java files

---
+++ Java/JCFNode.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (c) 2015 Clare Controls. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Clare Controls ("Confidential Information"). You
+ * shall not disclose or reproduce such Confidential Information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with Clare Controls.
+ */
+package com.clarecontrols.equator.solstice.api.beta;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.clarecontrols.equator.solstice.db.entities.rules.CFNode;
+import com.clarecontrols.equator.solstice.db.entities.rules.CFProvider;
+
+/**
+ * JCFNode represents export/import CFNode helper.
+ */
+final class JCFNode {
+
+    /**
+     * @param cfNodes CFNodes to export
+     * @param jsonObject json to export to
+     * @throws JSONException json exception
+     */
+    static void exportCFNodes(final Set<CFNode> cfNodes, final JSONObject jsonObject) throws JSONException {
+        Objects.requireNonNull(cfNodes);
+        Objects.requireNonNull(jsonObject);
+
+        // keys [cfnodes, cfnode_cfnode]
+        JUtils.getArray(JKey.CFNODES, jsonObject);
+        JUtils.getArray(JKey.CFNODE_CFNODE, jsonObject);
+        final Set<CFNode> exportedNodes = new HashSet<CFNode>();
+        for (final CFNode cfNode : cfNodes) {
+            JCFNode.exportCFNode(exportedNodes, cfNode, jsonObject);
+        }
+    }
+
+    /**
+     * @param exportedNodes exported nodes
+     * @param cfNode CFNode to export
+     * @param jsonObject json object to export to
+     * @throws JSONException json exception
+     */
+    private static void exportCFNode(final Set<CFNode> exportedNodes, final CFNode cfNode, final JSONObject jsonObject)
+        throws JSONException {
+        if (!exportedNodes.contains(cfNode)) {
+            final JSONObject json = new JSONObject();
+            // cfNode[ id, name, notes, uid ]
+            json.put(JKey.ID, cfNode.getId());
+            json.put(JKey.NAME, cfNode.getName());
+            json.put(JKey.NOTES, cfNode.getNotes());
+            json.put(JKey.UID, cfNode.getUid());
+
+            final JSONArray jsonProperties = JUtils.getArray(JKey.PROPERTIES, json);
+            for (final Map.Entry<String, Serializable> property : cfNode.getProperties().entrySet()) {
+                exportProperty(property, jsonProperties);
+            }
+
+            exportLookup(cfNode, json);
+            JUtils.getArray(JKey.CFNODES, jsonObject).put(json);
+            exportedNodes.add(cfNode);
+
+            // cfnode_cfnode
+            for (final CFNode child : cfNode.getChildren()) {
+                final JSONArray parentChild = new JSONArray();
+                parentChild.put(cfNode.getId()).put(child.getId());
+                JUtils.getArray(JKey.CFNODE_CFNODE, jsonObject).put(parentChild);
+                exportCFNode(exportedNodes, child, jsonObject);
+            }
+        }
+    }
+
+    /**
+     * @param property CFNode property to export
+     * @param jsonProperties json array to exprot to
+     * @throws JSONException json exception
+     */
+    private static void exportProperty(final Entry<String, Serializable> property, final JSONArray jsonProperties)
+        throws JSONException {
+        // reference: SerializerRules.getValueType() method
+        // keys [ key, value, type ]
+        final String key = property.getKey();
+        final Serializable value = property.getValue();
+        String type = null;
+        if (value instanceof Float) {
+            type = TYPE_FLOAT;
+        } else if (value instanceof Double) {
+            type = TYPE_DOUBLE;
+        } else if (value instanceof Boolean) {
+            type = TYPE_BOOLEAN;
+        } else if (value instanceof String) {
+            type = TYPE_STRING;
+        } else if (value instanceof Long) {
+            type = TYPE_LONG;
+        } else if (value instanceof Integer) {
+            type = TYPE_INT;
+        } else if (value instanceof String[]) {
+            type = TYPE_STRING_ARRAY;
+        }
+        final JSONObject json = new JSONObject();
+        json.put(JKey.KEY, key);
+        json.put(JKey.VALUE, value);
+        json.put(JKey.TYPE, type);
+        jsonProperties.put(json);
+    }
+
+    /**
+     * @param cfNode CFNode to export
+     * @param json json to export to
+     * @throws JSONException json exception
+     */
+    private static void exportLookup(final CFNode cfNode, final JSONObject json) throws JSONException {
+        // keys [ _providerName_, _providerTypeName_, _projectVersionId_ ]
+        final CFProvider provider = cfNode.getProvider();
+        if (provider != null) {
+            json.put(JKey._PROVIDER_NAME_, provider.getName());
+            json.put(JKey._PROVIDER_TYPE_NAME_, provider.getProviderType().name());
+        }
+        if (cfNode.getProjectVersion() != null) {
+            json.put(JKey._PROJECT_VERSION_ID_, cfNode.getProjectVersion().getId());
+        }
+    }
+
+    /**
+     * @param manager import manager
+     * @param jsonObject json to import CFNodes
+     * @throws JSONException json exception
+     */
+    static void importCFNodes(final ImportManager manager, final JSONObject jsonObject) throws JSONException {
+        Objects.requireNonNull(manager);
+        Objects.requireNonNull(jsonObject);
+
+        final Map<Integer, CFNode> idCFNodeMap = manager.getIdCFNodeMap();
+        idCFNodeMap.clear();
+        final JSONArray cfNodes = jsonObject.optJSONArray(JKey.CFNODES);
+        if (cfNodes != null) {
+            for (int index = 0, size = cfNodes.length(); index < size; index++) {
+                importCFNode(manager, cfNodes.getJSONObject(index));
+            }
+        }
+        // import cfnode_cfnode
+        final JSONArray nodeNode = jsonObject.optJSONArray(JKey.CFNODE_CFNODE);
+        if (nodeNode != null) {
+            for (int index = 0, size = nodeNode.length(); index < size; index++) {
+                final JSONArray parentChild = nodeNode.getJSONArray(index);
+                final CFNode parent = idCFNodeMap.get(parentChild.getInt(0));
+                final CFNode child = idCFNodeMap.get(parentChild.getInt(1));
+                child.setParent(parent);
+            }
+        }
+    }
+
+    /**
+     * @param manager import manager
+     * @param json json to import cfNode
+     * @throws JSONException json exception
+     */
+    private static void importCFNode(final ImportManager manager, final JSONObject json) throws JSONException {
+        final int cfNodeId = json.getInt(JKey.ID);
+        if (!manager.getIdCFNodeMap().containsKey(cfNodeId)) {
+            final CFNode cfNode = new CFNode();
+            cfNode.setName(json.optString(JKey.NAME, null));
+            cfNode.setNotes(json.optString(JKey.NOTES, null));
+            cfNode.setUid(json.optString(JKey.UID, null));
+            if (json.has(JKey._PROJECT_VERSION_ID_)) {
+                cfNode.setProjectVersion(manager.getVersion());
+            }
+            if (json.has(JKey._PROVIDER_NAME_)) {
+                cfNode.setProvider(manager.lookupCFProvider(json.optString(JKey._PROVIDER_NAME_, null), json.optString(
+                    JKey._PROVIDER_TYPE_NAME_, null)));
+            }
+            importProperties(cfNode, json.getJSONArray(JKey.PROPERTIES));
+            manager.getIdCFNodeMap().put(cfNodeId, cfNode);
+        }
+    }
+
+    /**
+     * @param cfNode CFNode to import properties to
+     * @param optJSONArray json array contains CFNode properties
+     * @throws JSONException json exception
+     */
+    private static void importProperties(final CFNode cfNode, final JSONArray jsonArray) throws JSONException {
+        for (int index = 0, size = jsonArray.length(); index < size; index++) {
+            final JSONObject json = jsonArray.getJSONObject(index);
+            final String key = json.optString(JKey.KEY, null);
+            final String value = json.optString(JKey.VALUE, null);
+            final String type = json.optString(JKey.TYPE, null);
+            if (TYPE_FLOAT.equals(type)) {
+                cfNode.getProperties().put(key, Float.parseFloat(value));
+            } else if (TYPE_DOUBLE.equals(type)) {
+                cfNode.getProperties().put(key, Double.parseDouble(value));
+            } else if (TYPE_BOOLEAN.equals(type)) {
+                cfNode.getProperties().put(key, Boolean.parseBoolean(value));
+            } else if (TYPE_STRING.equals(type)) {
+                cfNode.getProperties().put(key, value);
+            } else if (TYPE_LONG.equals(type)) {
+                cfNode.getProperties().put(key, Long.parseLong(value));
+            } else if (TYPE_INT.equals(type)) {
+                cfNode.getProperties().put(key, Integer.parseInt(value));
+            } else if (TYPE_STRING_ARRAY.equals(type)) {
+                cfNode.getProperties().put(key, JUtils.toStringArray(new JSONArray(value)));
+            } else {
+                cfNode.getProperties().put(key, value);
+            }
+        }
+    }
+
+    /**
+     * Private constructor.
+     */
+    private JCFNode() {
+        // Utility class
+    }
+
+    private static final String TYPE_FLOAT = "float";
+    private static final String TYPE_DOUBLE = "double";
+    private static final String TYPE_BOOLEAN = "boolean";
+    private static final String TYPE_STRING = "string";
+    private static final String TYPE_LONG = "long";
+    private static final String TYPE_INT = "int";
+    private static final String TYPE_STRING_ARRAY = "string[]";
+
+}
---
+++ Java/JDeviceItem.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright (c) 2015 Clare Controls. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Clare Controls ("Confidential Information"). You
+ * shall not disclose or reproduce such Confidential Information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with Clare Controls.
+ */
+package com.clarecontrols.equator.solstice.api.beta;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.clarecontrols.equator.solstice.db.entities.DeviceClass;
+import com.clarecontrols.equator.solstice.db.entities.DeviceItem;
+import com.clarecontrols.equator.solstice.db.entities.DeviceType;
+
+/**
+ * JDeviceItem represents export/import DeviceItem helper.
+ */
+final class JDeviceItem {
+
+    /**
+     * @param devices devices to export
+     * @param jsonObject json to export to
+     * @throws JSONException json exception
+     */
+    static void exportDeviceItems(final Set<DeviceItem> devices, final JSONObject jsonObject) throws JSONException {
+        Objects.requireNonNull(devices);
+        Objects.requireNonNull(jsonObject);
+
+        // keys [ devices, device_device ]
+        JUtils.getArray(JKey.DEVICES, jsonObject);
+        JUtils.getArray(JKey.DEVICE_DEVICE, jsonObject);
+        for (final DeviceItem device : devices) {
+            exportDeviceItem(device, jsonObject);
+        }
+    }
+
+    /**
+     * @param device device item to export
+     * @param jsonObject json to export to
+     * @throws JSONException json exception
+     */
+    private static void exportDeviceItem(final DeviceItem device, final JSONObject jsonObject) throws JSONException {
+        final JSONObject json = new JSONObject();
+        // device[ id, name, notes, uid, lastUpdate, vendor, version, modelNumber, troubleshooting, deviceItemsProps ]
+        // device[ protocolVerRange, ~template, ~hidden, ~equipment, ~certified ]
+        json.put(JKey.CERTIFIED, device.isCertified());
+        json.put(JKey.DEVICE_ITEMS_PROPS, device.getDeviceItemsProps());
+        json.put(JKey.EQUIPMENT, device.isEquipment());
+        json.put(JKey.HIDDEN, device.isHidden());
+        json.put(JKey.ID, device.getId());
+        json.put(JKey.MODEL_NUMBER, device.getModelNumber());
+        json.put(JKey.NAME, device.getName());
+        json.put(JKey.NOTES, device.getNotes());
+        json.put(JKey.PROTOCOL_VER_RANGE, device.getProtocolVerRange());
+        json.put(JKey.TEMPLATE, device.isTemplate());
+        json.put(JKey.TROUBLESHOOTING, device.getTroubleshooting());
+        json.put(JKey.UID, device.getUid());
+        json.put(JKey.VENDOR, device.getVendor());
+        json.put(JKey.VERSION, device.getVersion());
+        JUtils.putTimestamp(JKey.LAST_UPDATE, device.getLastUpdate(), json);
+
+        exportLookup(device, json);
+        JUtils.getArray(JKey.DEVICES, jsonObject).put(json);
+
+        // device_device
+        for (final DeviceItem child : device.getChildren()) {
+            final JSONArray parentChild = new JSONArray();
+            parentChild.put(device.getId()).put(child.getId());
+            JUtils.getArray(JKey.DEVICE_DEVICE, jsonObject).put(parentChild);
+        }
+    }
+
+    /**
+     * @param device device item to export
+     * @param json json to export to
+     * @throws JSONException json exception
+     */
+    private static void exportLookup(final DeviceItem device, final JSONObject json) throws JSONException {
+        // lookup [ _masterTemplateName_, _masterTemplateVendor_, _masterTemplateModelNumber_, _masterTemplateVersion_ ]
+        final DeviceItem template = device.getMasterTemplate();
+        if (template != null) {
+            json.put(JKey._MASTER_TEMPLATE_NAME_, template.getName());
+            json.put(JKey._MASTER_TEMPLATE_VENDOR_, template.getVendor());
+            json.put(JKey._MASTER_TEMPLATE_MODEL_NUMBER_, template.getModelNumber());
+            json.put(JKey._MASTER_TEMPLATE_VERSION_, template.getVersion());
+        }
+
+        // lookup [ _zoneId_, _lastUpdateUserEmail_ ]
+        if (device.getLastUpdateUser() != null) {
+            json.put(JKey._LAST_UPDATE_USER_EMAIL_, device.getLastUpdateUser().getEmail());
+        }
+        if (device.getZone() != null) {
+            json.put(JKey._ZONE_ID_, device.getZone().getId());
+        }
+
+        // lookup [ _deviceTypes_, _deviceTypeName_, _deviceCategoryName_ ]
+        final JSONArray jsonDeviceTypes = JUtils.getArray(JKey._DEVICE_TYPES_, json);
+        for (final DeviceType deviceType : device.getDeviceTypes()) {
+            final JSONObject jsonDeviceType = new JSONObject();
+            jsonDeviceType.put(JKey._DEVICE_TYPE_NAME_, deviceType.getName());
+            if (deviceType.getDeviceCategory() != null) {
+                jsonDeviceType.put(JKey._DEVICE_CATEGORY_NAME_, deviceType.getDeviceCategory().getName());
+            }
+            jsonDeviceTypes.put(jsonDeviceType);
+        }
+
+        // lookup [ _protocolAdapterName_, _protocolAdapterVersion_ ]
+        json.put(JKey._PROTOCOL_ADAPTER_NAME_, device.getProtocolAdapter().getName());
+        json.put(JKey._PROTOCOL_ADAPTER_VERSION_, device.getProtocolAdapter().getVersion());
+
+        // lookup [ _deviceClasses_ ]
+        final JSONArray jsonDeviceClasses = JUtils.getArray(JKey._DEVICE_CLASSES_, json);
+        for (final DeviceClass deviceClass : device.getDeviceClasses()) {
+            jsonDeviceClasses.put(deviceClass.getName());
+        }
+    }
+
+    /**
+     * @param manager import manager
+     * @param jsonObject json object to import
+     * @throws JSONException json exception
+     */
+    static void importDeviceItems(final ImportManager manager, final JSONObject jsonObject) throws JSONException {
+        Objects.requireNonNull(manager);
+        Objects.requireNonNull(jsonObject);
+
+        final Map<Integer, DeviceItem> idDeviceMap = manager.getIdDeviceMap();
+        idDeviceMap.clear();
+        final JSONArray devices = jsonObject.optJSONArray(JKey.DEVICES);
+        if (devices != null) {
+            for (int index = 0, size = devices.length(); index < size; index++) {
+                importDeviceItem(manager, devices.getJSONObject(index));
+            }
+        }
+        // import device_device
+        final JSONArray deviceDevices = jsonObject.optJSONArray(JKey.DEVICE_DEVICE);
+        if (deviceDevices != null) {
+            for (int index = 0, size = deviceDevices.length(); index < size; index++) {
+                final JSONArray parentChild = deviceDevices.getJSONArray(index);
+                final DeviceItem parent = idDeviceMap.get(parentChild.getInt(0));
+                final DeviceItem child = idDeviceMap.get(parentChild.getInt(1));
+                // parent.getChildren().add(child); // this way will not work right.
+                child.setParent(parent);
+            }
+        }
+    }
+
+    /**
+     * @param manager import manager
+     * @param json json object to import from
+     * @throws JSONException json exception
+     */
+    private static void importDeviceItem(final ImportManager manager, final JSONObject json) throws JSONException {
+        final Integer idDevice = json.getInt(JKey.ID);
+        if (!manager.getIdDeviceMap().containsKey(idDevice)) {
+            final DeviceItem device = new DeviceItem();
+            device.setCertified(json.optBoolean(JKey.CERTIFIED));
+            device.setDeviceItemsProps(JUtils.optStringStringMap(JKey.DEVICE_ITEMS_PROPS, json));
+            device.setEquipment(json.optBoolean(JKey.EQUIPMENT));
+            device.setHidden(json.optBoolean(JKey.HIDDEN));
+            device.setLastUpdate(JUtils.optTimestamp(JKey.LAST_UPDATE, json));
+            device.setModelNumber(json.optString(JKey.MODEL_NUMBER, null));
+            device.setName(json.optString(JKey.NAME, null));
+            device.setNotes(json.optString(JKey.NOTES, null));
+            device.setProtocolVerRange(json.optString(JKey.PROTOCOL_VER_RANGE, null));
+            device.setTemplate(json.optBoolean(JKey.TEMPLATE));
+            device.setTroubleshooting(json.optString(JKey.TROUBLESHOOTING, null));
+            device.setUid(json.optString(JKey.UID, null));
+            device.setVendor(json.optString(JKey.VENDOR, null));
+            device.setVersion(json.optString(JKey.VERSION, null));
+            importLookup(device, manager, json);
+            device.setProjectVersion(manager.getVersion());
+            manager.getIdDeviceMap().put(idDevice, device);
+        }
+    }
+
+    /**
+     * @param device device to import
+     * @param manager import manager
+     * @param json json to import from
+     * @param uidZoneMap zone-uid, zone map
+     * @throws JSONException
+     */
+    private static void importLookup(final DeviceItem device, final ImportManager manager, final JSONObject json)
+        throws JSONException {
+        // key [ _importTemplateId_ ]
+        if (json.has(JKey._IMPORT_TEMPLATE_ID_)) {
+            device.setMasterTemplate(manager.findEntity(DeviceItem.class, json.getInt(JKey._IMPORT_TEMPLATE_ID_)));
+        } else if (json.has(JKey._MASTER_TEMPLATE_NAME_)) {
+            final String name = json.optString(JKey._MASTER_TEMPLATE_NAME_, null);
+            final String vendor = json.optString(JKey._MASTER_TEMPLATE_VENDOR_, null);
+            final String modelNumber = json.optString(JKey._MASTER_TEMPLATE_MODEL_NUMBER_, null);
+            final String version = json.optString(JKey._MASTER_TEMPLATE_VERSION_, null);
+            device.setMasterTemplate(manager.lookupTemplate(name, vendor, modelNumber, version));
+        }
+        if (json.has(JKey._ZONE_ID_)) {
+            device.setZone(manager.getIdZoneMap().get(json.getInt(JKey._ZONE_ID_)));
+        }
+        device.setLastUpdateUser(manager.lookupUser(json.optString(JKey._LAST_UPDATE_USER_EMAIL_, null)));
+        final JSONArray jsonDeviceTypes = json.optJSONArray(JKey._DEVICE_TYPES_);
+        if (jsonDeviceTypes != null) {
+            for (int index = 0, size = jsonDeviceTypes.length(); index < size; index++) {
+                final JSONObject jsonDeviceType = jsonDeviceTypes.getJSONObject(index);
+                final DeviceType deviceType =
+                    manager.lookupDeviceType(jsonDeviceType.optString(JKey._DEVICE_TYPE_NAME_, null), jsonDeviceType
+                        .optString(JKey._DEVICE_CATEGORY_NAME_, null));
+                if (deviceType != null) {
+                    device.getDeviceTypes().add(deviceType);
+                }
+            }
+        }
+        device.setProtocolAdapter(manager.lookupProtocolAdapter(json.optString(JKey._PROTOCOL_ADAPTER_NAME_, null),
+            json.optString(JKey._PROTOCOL_ADAPTER_VERSION_, null)));
+        final JSONArray jsonDeviceClasses = json.optJSONArray(JKey._DEVICE_CLASSES_);
+        if (jsonDeviceClasses != null) {
+            for (int index = 0, size = jsonDeviceClasses.length(); index < size; index++) {
+                device.getDeviceClasses().add(manager.lookupDeviceClass(jsonDeviceClasses.optString(index, null)));
+            }
+        }
+    }
+
+    /**
+     * Private constructor.
+     */
+    private JDeviceItem() {
+        // Utility class
+    }
+}
